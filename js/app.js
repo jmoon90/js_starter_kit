94e6d94f2ac10c6786de247fe00dc4ae8c6d0077
John Moon <johnmoonyy@gmail.com>
2014-01-09 11:27:24 -0500

    Add Initial user guess and alert output

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-alert('Hey find me to know where to put the javascript');
+var max = 10
+alert('Guess a number between 1 and' + max);
+
+
+$('[data-guess]').on('submit', function() {
+  var passThis = $('input[name="user-guess"]').val();
+
+  if(passThis < 5) {
+    alert(passthis + ' was too low');
+  } else if(passThis === 6) {
+    alert("Congratulations, you've guessed the number in num guesses!");
+  } else if(passThis > 6 && passThis < 11 ) {
+    alert(passthis + ' was too high');
+  } else {
+    alert('Invalid input, must enter a number between 0 and ' + max);
+  };
+});
+
