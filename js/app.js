d771f6b3e5e1ad35c40ea89a2e4e553f82b8c47e
John Moon <johnmoonyy@gmail.com>
2014-01-12 20:05:06 -0500

    Add reset button to reload page

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
-var max = 10
+var max= Math.floor(Math.random() * 100);
 alert('Guess a number between 1 and' + max);
 
-
-$('[data-guess]').on('submit', function() {
+$('[data-guess]').on('submit', function(event) {
+  event.preventDefault();
   var passThis = $('input[name="user-guess"]').val();
 
-  if(passThis < 5) {
-    alert(passthis + ' was too low');
-  } else if(passThis === 6) {
+  if(passThis < Math.floor(max/2-1)) {
+    alert(passThis + ' was too low');
+  } else if(passThis == Math.floor(max/2)) {
     alert("Congratulations, you've guessed the number in num guesses!");
-  } else if(passThis > 6 && passThis < 11 ) {
-    alert(passthis + ' was too high');
+  } else if(passThis > Math.floor(max/2) && passThis <= max  ) {
+    alert(passThis + ' was too high');
   } else {
     alert('Invalid input, must enter a number between 0 and ' + max);
   };
 });
 
+function myFunction() {
+  window.location.reload();
+}
